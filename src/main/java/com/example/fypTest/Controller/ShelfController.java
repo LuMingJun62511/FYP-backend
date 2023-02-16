f6366c62e538cdb7263751a93568545f9be7ed7e
Fan Yikun <LuMingJun71543@outlook.com>
2023-02-16 21:52:38 +0000

    shelf manage

--- src/main/java/com/example/fypTest/Controller/ShelfController.java
+++ src/main/java/com/example/fypTest/Controller/ShelfController.java
@@ -1,19 +1,38 @@
 
 package com.example.fypTest.Controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.example.fypTest.DAO.SmsShelfItemRepository;
+import com.example.fypTest.DAO.SmsShelfRepository;
+import com.example.fypTest.Model.SmsShelfItem;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 
-@Controller
+@RestController
+@RequestMapping(value = "/sms")
 public class ShelfController {
 
-
-    @GetMapping("/ShelfManagement")
-    public String handleJumpShelf() throws Exception{
-        return "ShelfManagement/shelfManagementPage";
+    @Resource
+    private SmsShelfItemRepository testShelfItems;
+    @RequestMapping(value = "/ShelfManagement")
+    public List<SmsShelfItem> getMatchedRows(){
+        return testShelfItems.findByShelf_IdEquals(10);
     }
 
+    @Resource
+    private SmsShelfRepository testShelf;
+    @RequestMapping(value = "/ShelfBasicInfoRow")
+    public int getBasicInfoRow(){
+        return testShelf.findByIdEquals(10).getRowNum();
+    }
 
+    @RequestMapping(value = "/ShelfBasicInfoCol")
+    public int getBasicInfoCol(){
+        return testShelf.findByIdEquals(10).getColNum();
+    }
 
 }

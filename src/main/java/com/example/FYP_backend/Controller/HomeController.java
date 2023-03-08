8a3d050fc3e1ffa5aa041da562477cf46d776615
Fan Yikun <LuMingJun71543@outlook.com>
2023-03-08 20:26:12 +0000

    refactor

--- src/main/java/com/example/FYP_backend/Controller/HomeController.java
+++ src/main/java/com/example/FYP_backend/Controller/HomeController.java
@@ -1,8 +1,12 @@
 package com.example.FYP_backend.Controller;
 
 import com.example.FYP_backend.DAO.OmsOrderRepository;
+import com.example.FYP_backend.DAO.PmsAbstractProductRepository;
+import com.example.FYP_backend.DAO.PmsBatchRepository;
 import com.example.FYP_backend.Model.ChartOrderData;
 import com.example.FYP_backend.Model.OmsOrder;
+import com.example.FYP_backend.Model.PmsAbstractProduct;
+import com.example.FYP_backend.Model.PmsBatch;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,11 +39,14 @@ public class HomeController {
     Instant eightSettlementDateAgo = startOfWeekInstant.minus(Duration.ofDays(49));
     @Resource
     private OmsOrderRepository orderRepo;
-    @RequestMapping("/orderData")
+    @Resource
+    private PmsAbstractProductRepository productRepo;
+    @Resource
+    private PmsBatchRepository batchRepo;
+    @RequestMapping("/orderDataForChart")
     public List<ChartOrderData> orderData() {
         List<ChartOrderData> dataList = new LinkedList<>();
-        //8周
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 8; i++) {//8周
             dataList.add(new ChartOrderData(i-7,0.0,0));
         }
 
@@ -51,6 +58,34 @@ public class HomeController {
             changed.setAmount(changed.getAmount()+1);
         }
         return dataList;
+    }
+
+    @RequestMapping("/checkProducts")
+    public void checkProducts(){
+        Instant aWeekLater = now.plus(Duration.ofDays(7));//一周后的现在
+        //这个就是检查刷新，再返回商品函数，
+        List<PmsAbstractProduct> products= productRepo.findAll();
+        for (PmsAbstractProduct product:products) {
+//            判断一下货物是不是不够了
+            if(product.getStock() - product.getToBeOutbound() <= product.getLowStock()){
+                product.setIsLow(1);
+            }
+            List<PmsBatch> batches = batchRepo.findByAbstractProduct_IdEquals(product.getId());
+            for (PmsBatch batch : batches) {
+                if(batch.getBbd().isBefore(aWeekLater)){//如果有bbd在一周以内，那就很危险了，
+                    product.setIsUrgent(1);
+                }
+            }
+            //比完了记得把这个batches推上去，
+            batchRepo.saveAll(batches);
+        }
+        //比完了记得把这个products推上去，
+        productRepo.saveAll(products);
+    }
+
+    @RequestMapping("/getProducts")
+    public List<PmsAbstractProduct> getProducts(){
+        return productRepo.findAll();
     }
 
     public int countWeeksBeforeNow(Instant a) {
